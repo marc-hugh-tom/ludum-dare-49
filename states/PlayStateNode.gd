b26e51ef522e57500efec757bb1ad9219c58c0e8
Marc Jones <m4rcj0n35@gmail.com>
2021-10-04 20:34:29 +0100

    Tried to fix viewport scaling

--- states/PlayStateNode.gd
+++ states/PlayStateNode.gd
@@ -5,3 +5,12 @@ signal quit
 
 func _ready():
 	$MainViewport/Viewport.connect("quit", self, "emit_signal", ["quit"])
+	get_viewport().connect("size_changed", self, "viewport_size_changed")
+	viewport_size_changed()
+
+func viewport_size_changed():
+	var parent_vp = get_viewport()
+	var child_vp = $MainViewport/Viewport
+	child_vp.set_size(parent_vp.get_size())
+	child_vp.set_size_override_stretch(true)
+	child_vp.set_size_override(true, parent_vp.get_size_override())
